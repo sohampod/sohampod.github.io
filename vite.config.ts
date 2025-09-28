a03d8c76937dfe1b32c8f0fc9e8ed45696eec56b
Soham Poddar <soham.poddar23@gmail.com>
2025-09-29 00:25:21 +0200

    ghpage

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Change to root for custom domain
+})
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
